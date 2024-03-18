cd622def725e32e98c77f20542a7f7e432acbc82
Marco Kleimaier <marco2.kleimaier@gmx.de>
2024-03-18 11:33:20 +0100

    changed frontend texts

--- rag_frontend.py
+++ rag_frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import rag_backend as backend
 
 
-st.title("RAG - Red And Green... sth like that ;)")
+st.title("RAG - Chat with the 'Attention is all you Need'-Paper by Google!")
 
 if "vector_index" not in st.session_state:
     with st.spinner("Woooohoooo... Magic is happening..."):
@@ -13,10 +13,9 @@ if "llm" not in st.session_state:
 
 input_text = st.text_input("Questione...")
 if input_text:
-    with st.spinner("Coole Fakten, tolle KI, sexy Entwickler... ;)"):
-        response = backend.rag_response(
-            index=st.session_state.vector_index,
-            question=input_text,
-            llm=st.session_state.llm
-        )
-        st.write(response)
+    response = backend.rag_response(
+        index=st.session_state.vector_index,
+        question=input_text,
+        llm=st.session_state.llm
+    )
+    st.write(response)
